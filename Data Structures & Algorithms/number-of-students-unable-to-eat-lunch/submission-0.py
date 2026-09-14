829f4cda15027f0badb2f4233cd393ad9d2a7115
William Petrik <87589017+bpp123456789@users.noreply.github.com>
2026-09-14 15:47:59 -0400

    Add: number-of-students-unable-to-eat-lunch - submission-0

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        count = 0
+        while (count < len(students)):
+            student = students.pop(0)
+            if student == sandwiches[0]:
+                sandwiches.pop(0)
+                count = 0
+            else:
+                students.append(student)
+                count += 1
+        return count
+        
